9fe329bd5aa27ee01b9b3d479190dbd1851e7831
nam2nam <connectman11@gmail.com>
2020-08-04 23:41:43 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <header className="App-header">
+        <img src={logo} alt="logo" />
+        <h2>렛츠 관리 시스템~!</h2>
+        <p>
+          안늉 나는 화면
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
