607dbeadf6fdb2b2f87c9e29d442507bc217a1d7
nam2nam <connectman11@gmail.com>
2020-08-06 23:18:22 +0900

    create js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Customer from './components/Customer'
 
-function App() {
+const customers = [
+  {
+    'id':1,
+    'img' : 'https://lh3.googleusercontent.com/ygBeB4Ug1roppIZvrijsesps7NX0LRLrgY2Pt8_QRYHsetC0tGQR0l7mmm2CtRUrONNF91A=s151',
+    'name' : '승ㅎ남',
+    'birthday' : '910201',
+    'gender' : '여',
+    'job' : '회사원'
+  },
+  {
+    'id':2,
+    'img' : 'https://lh3.googleusercontent.com/ygBeB4Ug1roppIZvrijsesps7NX0LRLrgY2Pt8_QRYHsetC0tGQR0l7mmm2CtRUrONNF91A=s151',
+    'name' : '승남2',
+    'birthday' : '910201',
+    'gender' : '여',
+    'job' : '회사원'
+  },
+  {
+    'id':3,
+    'img' : 'https://lh3.googleusercontent.com/ygBeB4Ug1roppIZvrijsesps7NX0LRLrgY2Pt8_QRYHsetC0tGQR0l7mmm2CtRUrONNF91A=s151',
+    'name' : '승남3',
+    'birthday' : '910201',
+    'gender' : '여',
+    'job' : '회사원'
+  }
+]
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        {
+          customers.map(c => {
+            return(
+              <Customer
+                key={c.id}
+                id={c.id}
+                img={c.img}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          })
+        }
+        
+      </div>
+    )
+  }
+}
+
+/*
+function App() { 
   return (
     <div className="gray-background">
       <header className="App-header">
-        <img src={logo} alt="logo" />
         <h2>렛츠 관리 시스템~!</h2>
         <p>
           안늉 나는 화면
@@ -15,5 +67,5 @@ function App() {
     </div>
   );
 }
-
+ */
 export default App;
